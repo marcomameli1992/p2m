34cc9d6603c14dba22c1541749c6dea57ceb0680
marcomameli <mameli.1.marco@gmail.com>
2022-07-24 09:49:20 +0200

    update features count

--- model/mesh/mesh_deformation_gat.py
+++ model/mesh/mesh_deformation_gat.py
@@ -30,16 +30,17 @@ class MeshDeformationBlock(nn.Module):
             'google/vit-large-patch16-384': 48,
             'google/vit-huge-patch14-224-in21k': 48,
             'microsoft/swinv2-base-patch4-window12to24-192to384-22kto1k-ft': 48,
-            'microsoft/swinv2-large-patch4-window12to16-192to256-22kto1k-ft': 48,
-            'microsoft/swinv2-large-patch4-window12-192-22k': 48,
-            'microsoft/swinv2-small-patch4-window8-256': 48,
-            'microsoft/swinv2-base-patch4-window12to16-192to256-22kto1k-ft': 48,
-            'microsoft/swinv2-base-patch4-window16-256': 48,
-            'microsoft/swinv2-base-patch4-window12-192-22k': 48,
-            'microsoft/swinv2-base-patch4-window8-256': 48,
-            'microsoft/swinv2-small-patch4-window16-256': 48,
-            'microsoft/swinv2-tiny-patch4-window16-256': 48,
-            'microsoft/swinv2-tiny-patch4-window8-256': 48,
+            'microsoft/swinv2-large-patch4-window12to16-192to256-22kto1k-ft': 36,
+            'microsoft/swinv2-large-patch4-window12-192-22k': 36,
+            'microsoft/swinv2-small-patch4-window8-256': 36,
+            'microsoft/swinv2-base-patch4-window12to16-192to256-22kto1k-ft': 36,
+            'microsoft/swinv2-base-patch4-window16-256': 36,
+            'microsoft/swinv2-base-patch4-window12-192-22k': 36,
+            'microsoft/swinv2-base-patch4-window8-256': 36,
+            'microsoft/swinv2-small-patch4-window16-256': 36,
+            'microsoft/swinv2-tiny-patch4-window16-256': 36,
+            'microsoft/swinv2-tiny-patch4-window8-256': 39
+            ,
         }
 
         self.conv1 = GATv2Conv(feature_dict[transformer_name] + feature_shape_dim, 1024, heads=1, concat=False)
